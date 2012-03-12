4d4960720d0bcf714618849d3d9173c6f5173b42
msieradzki <michal.sieradzki@blueservices.pl>
2012-03-12 10:34:26 +0100

    fix image templat taga

--- filemanager/templatetags/filemanager.py
+++ filemanager/templatetags/filemanager.py
@@ -6,5 +6,4 @@ register = template.Library()
 
 @register.simple_tag
 def image(static_file, size, crop=None):
-    return static_file.image_path(size, crop)
-    
+    return static_file.image_path(size, crop) if static_file else None
